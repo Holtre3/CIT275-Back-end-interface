8fce85d779be63a7014f8447e8c79f05c5699c28
Holtre3 <holtre3@mail.nmc.edu>
2017-02-27 10:18:46 -0500

    Added Entity Compatible Model

---
+++ CIT275-Back-end-interface/CIT275-Back-end-interface/Models/AssetType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CIT275_Back_end_interface.Models
+{
+    public class AssetType
+    {   [StringLength(50)]
+        [Required]
+        public string Name { get; set; }
+        [StringLength(500)]
+        public string Description { get; set; }
+        [Key]
+        public int ID { get; set; }
+
+    }
+}
