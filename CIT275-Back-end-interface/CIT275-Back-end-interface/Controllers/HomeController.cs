d6def45ba43721c171233895baf70a13830cbddb
Holtre3 <holtre3@mail.nmc.edu>
2017-02-27 10:08:01 -0500

    Test Commit

--- CIT275-Back-end-interface/CIT275-Back-end-interface/Controllers/HomeController.cs
+++ CIT275-Back-end-interface/CIT275-Back-end-interface/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace CIT275_Back_end_interface.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
