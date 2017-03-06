29d405c6c6fc3324ee526d7a7738f3a176246a0f
Sam Lebel <lebels@mail.nmc.edu>
2017-03-06 11:17:30 -0500

    LogFile Table Sorting
    Added JS, CSS, and Assets for sorting tables.

--- CIT275-Back-end-interface/CIT275-Back-end-interface/Scripts/_references.js
+++ CIT275-Back-end-interface/CIT275-Back-end-interface/Scripts/_references.js
@@ -3,5 +3,6 @@
 /// <autosync enabled="true" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
+/// <reference path="jquery.tablesorter.min.js" />
 /// <reference path="bootstrap.js" />
 /// <reference path="respond.js" />
