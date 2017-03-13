b15ba0dffbbfe63418b0921c37664493ae9cb00b
Holtre3 <holtre3@mail.nmc.edu>
2017-03-13 11:40:42 -0400

    Fixed(?) routing for Assets page.

--- CIT275-Back-end-interface/CIT275-Back-end-interface/App_Start/RouteConfig.cs
+++ CIT275-Back-end-interface/CIT275-Back-end-interface/App_Start/RouteConfig.cs
@@ -13,11 +13,14 @@ namespace CIT275_Back_end_interface
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.RouteExistingFiles = true;
         }
     }
 }
